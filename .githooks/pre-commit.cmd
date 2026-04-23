a6426f9bd235c767101219feed3646421c43b6c3
golemjbc <adam@bednarovi.org>
2026-04-23 12:23:53 +0200

    Pridat automaticke zvysovani verze

--- .githooks/pre-commit.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-set "REPO_ROOT=%~dp0..\"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%REPO_ROOT%scripts\bump-version.ps1"
-if errorlevel 1 exit /b 1
-
-git add index.html
-exit /b 0
